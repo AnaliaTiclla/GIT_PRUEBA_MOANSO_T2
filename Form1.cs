fe94350df3fe788de5e02d47665f58ebe2ec4d46
AnaliaTiclla <146894860+AnaliaTiclla@users.noreply.github.com>
2023-10-04 07:43:10 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace EJEMPLO_MOANSO
+namespace calculadoramoanso
 {
     public partial class Form1 : Form
     {
@@ -17,7 +7,6 @@ namespace EJEMPLO_MOANSO
             InitializeComponent();
         }
 
-
         private void LIMPIAR_Click(object sender, EventArgs e)
         {
             txtN1.Text = "";
@@ -25,7 +14,19 @@ namespace EJEMPLO_MOANSO
             txtR.Text = "";
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double N1, N2, R;
+            N1 = double.Parse(txtN1.Text);
+            N2 = double.Parse(txtN2.Text);
+            R = N1 + N2;
+            txtR.Text = R.ToString();
 
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
