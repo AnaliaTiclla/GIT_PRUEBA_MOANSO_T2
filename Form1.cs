fd1a95cd54f148f24ec11632a8abb14125ac1bed
AnaliaTiclla <146894860+AnaliaTiclla@users.noreply.github.com>
2023-10-04 07:41:29 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -25,20 +25,7 @@ namespace EJEMPLO_MOANSO
             txtR.Text = "";
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            double N1, N2, R;
-            N1 = double.Parse(txtN1.Text);
-            N2 = double.Parse(txtN2.Text);
-            R = N1 + N2;
-            txtR.Text = R.ToString();
 
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
 
     }
 }
